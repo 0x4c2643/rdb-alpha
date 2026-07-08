8dc0f1215be943a46818fb1044663e4a7309f815
0x4c2643 <lc25-1308@lclark.edu>
2026-07-08 17:09:55 -0400

    Save progress: 1030.1

--- insert_data.sh
+++ insert_data.sh
@@ -43,11 +43,11 @@ do
   fi
 done
 cat students_test.csv | while IFS=","  read FIRST LAST MAJOR GPA
-
 do
 if [[ $FIRST != first_name ]]
 then
 	# get major_id
+	MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 	# if not found
 	# set to null
 	# insert student
